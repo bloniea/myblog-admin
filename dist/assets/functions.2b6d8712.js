6327e90bba614c964100dc9a941248d4b37e4ab1
bloniea <delishalovelyyo@qq.com>
2022-03-01 17:04:43 +0800

    add dist

---
+++ dist/assets/functions.2b6d8712.js
@@ -0,0 +1,1 @@
+const l=t=>{var e=new Date(t*1e3),a=e.getFullYear()+"-",r=(e.getMonth()+1<10?"0"+(e.getMonth()+1):e.getMonth()+1)+"-",g=(e.getDate()+1<10?"0"+(e.getDate()+1):e.getDate()+1)+" ",o=e.getHours()+":",v=e.getMinutes()+":",s=e.getSeconds();return a+r+g+o+v+s};export{l as s};
